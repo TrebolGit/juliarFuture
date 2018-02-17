71b29f9e620c24148ae326dadf628c7fd667ed6b
TheAndreiM <andreifundrei@gmail.com>
2018-02-16 23:39:45 -0500

    reverted back due to incompatibility with JuliarLib

--- src/main/java/com/juliar/errors/JuliarLogger.java
+++ src/main/java/com/juliar/errors/JuliarLogger.java
@@ -1,8 +1,6 @@
 package com.juliar.errors;
 
-import com.bugsnag.Bugsnag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static java.lang.System.*;
 
 /**
  * Created by AndreiM on 12/27/2016.
@@ -11,31 +9,26 @@ public class JuliarLogger {
     private static boolean hasError = false;
     private static int errors = 0;
 
-    final static Logger logger = LoggerFactory.getLogger(JuliarLogger.class);
-    final static Bugsnag bugsnag = new Bugsnag("c7e03c1e69143ad2fb1f3ea13ed8fda0");
-
-    //bugsnag.addCallback(report ->report.addToTab("subsystem", "name", "2018"));
-    //bugsnag.notify(new RuntimeException("Initiated"));
 
     public static boolean hasErrors(){
         return hasError;
     }
 
     public static void log(String msg){
-        logger.debug(msg);
+        out.println(msg);
     }
 
     public static void log(String msg,Exception e){
-        logger.debug(msg + e.toString());
+        out.println(msg);
     }
 
     public static void log(Exception e){
-        logger.debug(e.toString());
+        e.printStackTrace( err );
     }
 
 
     public static void logerr(String msg){
-        logger.debug(msg);
+        err.println(msg);
     }
 
     public static void errorFound(){
@@ -49,20 +42,20 @@ public class JuliarLogger {
 
     public static void exitIfErrors(){
         if(JuliarLogger.hasError){
-            logger.debug("Found $errors errors!");
+            err.println("Found "+errors+" errors!");
             throw new NullPointerException();
         }
     }
 
     public JuliarLogger(String Message){
-        logger.debug("Error: " + Message);
+        out.println("Error: " + Message);
     }
 
     public JuliarLogger(String Message, Exception Type){
-        logger.debug("Error: " + Message);
+        out.println("Error: " + Message);
     }
 
     public static void message(String message){
-        logger.debug( message );
+        out.println( message );
     }
 }
